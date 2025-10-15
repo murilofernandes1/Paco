2e34752981204937b0f04c945e25c3cfc4075de1
Murilo <murigod8@gmail.com>
2025-10-15 19:59:16 -0300

    feat: Error response and loading spinner component added

--- ecommerce/src/screens/Auth/Login/Login.tsx
+++ ecommerce/src/screens/Auth/Login/Login.tsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
 import api from "../../../services/api";
 import styles from "./Login.module.css";
-import { useNavigate } from "react-router-dom";
+import Spinner from "../../../components/Spinner/Spinner";
 export default function Login() {
   const navigate = useNavigate();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
 
   async function HandleLogin(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
@@ -16,32 +18,63 @@ export default function Login() {
         email: email,
         password: password,
       });
-      localStorage.setItem(token, "token");
+      localStorage.setItem("token", token);
       alert("Usuário logado com sucesso");
       navigate("/");
     } catch (error) {
-      alert("Email ou senha inválidos");
+      setIsLoading(false);
+      setError(true);
+
       console.log(error);
     }
   }
   return (
-    <div className={styles.container}>
-      <form className={styles.form} onSubmit={HandleLogin}>
-        <input
-          className={styles.input}
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="Digite seu email"
-        />
-        <input
-          className={styles.input}
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder="Digite sua senha"
-        />
-        <button>Entrar</button>
-      </form>
-      {isLoading ? <p>Ta carregando</p> : <div />}
-    </div>
+    <>
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <div className={styles.container}>
+          <form className={styles.form} onSubmit={HandleLogin}>
+            <h1 className={styles.title}>Entrar</h1>
+            <div className={styles.inputcontainer}>
+              <p className={styles.label}>Email</p>
+              <input
+                className={styles.input}
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="Digite seu email"
+                required
+              />
+              <p className={styles.label}>Senha</p>
+              <input
+                className={styles.input}
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Digite sua senha"
+                required
+              />
+            </div>
+            <button>Entrar</button>
+            <div className={styles.auth}>
+              <p className={styles.message}>
+                Não tem uma conta?{" "}
+                <span>
+                  <Link className={styles.link} to="/register">
+                    Registre-se
+                  </Link>
+                </span>
+              </p>
+            </div>
+            {error ? (
+              <p className={`${styles.error} ${error ? styles.show : ""}`}>
+                Email ou senha inválidos.
+              </p>
+            ) : (
+              <div />
+            )}
+          </form>
+        </div>
+      )}
+    </>
   );
 }
--- ecommerce/src/screens/Auth/Register/Register.tsx
+++ ecommerce/src/screens/Auth/Register/Register.tsx
@@ -1,7 +1,89 @@
+import { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import Spinner from "../../../components/Spinner/Spinner";
+import api from "../../../services/api";
+import styles from "./Register.module.css";
 export default function Register() {
+  const navigate = useNavigate();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  async function HandleLogin(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    setIsLoading(true);
+    try {
+      const { data: token } = await api.post("/users/register", {
+        name: name,
+        email: email,
+        password: password,
+      });
+      localStorage.setItem(token, "token");
+      alert("Usuário registrado com sucesso");
+      navigate("/");
+    } catch (error) {
+      setIsLoading(false);
+      setError(true);
+      console.log(error);
+    }
+  }
   return (
-    <div>
-      <p>Tela de cadastro</p>
-    </div>
+    <>
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <div className={styles.container}>
+          <form className={styles.form} onSubmit={HandleLogin}>
+            <h1 className={styles.title}>Registrar-se</h1>
+            <div className={styles.inputcontainer}>
+              <p className={styles.label}>Nome</p>
+              <input
+                className={styles.input}
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                placeholder="Digite seu nome"
+                required
+              />
+              <p className={styles.label}>Email</p>
+              <input
+                className={styles.input}
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="Digite seu email"
+                required
+              />
+              <p className={styles.label}>Senha</p>
+              <input
+                className={styles.input}
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Digite sua senha"
+                required
+              />
+            </div>
+            <button>Entrar</button>
+            <div className={styles.auth}>
+              <p className={styles.message}>
+                Já tem uma conta?{" "}
+                <span>
+                  <Link className={styles.link} to="/login">
+                    Entrar
+                  </Link>
+                </span>
+              </p>
+            </div>
+            {error ? (
+              <p className={`${styles.error} ${error ? styles.show : ""}`}>
+                Email ou senha inválidos.
+              </p>
+            ) : (
+              <div />
+            )}
+          </form>
+        </div>
+      )}
+    </>
   );
 }
