2692abb840249d9627f25c6f413171c0cf4950f9
Murilo <murigod8@gmail.com>
2025-10-15 18:44:57 -0300

    feat: Logins's page added

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,8 +2,10 @@ import express from "express";
 import userRoutes from "./routes/userRoutes.js";
 import productsRoutes from "./routes/productsRoutes.js";
 import ordersRoutes from "./routes/ordersRoutes.js";
+import Cors from "cors";
 const app = express();
 app.use(express.json());
+app.use(Cors());
 
 app.use("/users", userRoutes);
 app.use("/products", productsRoutes);
--- ecommerce/src/screens/Auth/Login/Login.tsx
+++ ecommerce/src/screens/Auth/Login/Login.tsx
@@ -1,7 +1,47 @@
+import { useState } from "react";
+import api from "../../../services/api";
+import styles from "./Login.module.css";
+import { useNavigate } from "react-router-dom";
 export default function Login() {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  async function HandleLogin(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    setIsLoading(true);
+    try {
+      const { data: token } = await api.post("/users/login", {
+        email: email,
+        password: password,
+      });
+      localStorage.setItem(token, "token");
+      alert("Usuário logado com sucesso");
+      navigate("/");
+    } catch (error) {
+      alert("Email ou senha inválidos");
+      console.log(error);
+    }
+  }
   return (
-    <div>
-      <p>Tela de login</p>
+    <div className={styles.container}>
+      <form className={styles.form} onSubmit={HandleLogin}>
+        <input
+          className={styles.input}
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Digite seu email"
+        />
+        <input
+          className={styles.input}
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Digite sua senha"
+        />
+        <button>Entrar</button>
+      </form>
+      {isLoading ? <p>Ta carregando</p> : <div />}
     </div>
   );
 }
